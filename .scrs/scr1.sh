d50f1545a73a5a241789a862200ce5dda15b454c
Rellikeht <michaelhemperek@gmail.com>
2021-01-31 09:35:32 +0100

    keynav intensifies

--- .scrs/scr1.sh
+++ .scrs/scr1.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xclip -o | xclip -selection clipboard -i
+#xclip -o | xclip -selection clipboard -i
+xclip -o | xargs `dmenu_path | dmenu`
--- .scrs/scr2.sh
+++ .scrs/scr2.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xdotool mousemove 0 0
-echo "^ca(1, echo 1)  1  ^ca()\n^ca(2, st)  2  ^ca()" | dzen2 -p -m h -l 2 -w 100 -h 30 # -e "entermaster=grabkeys;enterslave=grabkeys;key_Escape=exit;key_Super_L=exec:sh ~/.scrs/ststart.sh;"
+wget -O - `xclip -o` | `dmenu_path | dmenu` -
