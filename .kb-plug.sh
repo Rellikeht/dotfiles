85c8510fc813f829913c3ea666686d4db4536b7c
Rellikeht <michaelhemperek@gmail.com>
2022-07-03 13:41:03 +0200

    altgr usage intensifies

--- .kb-plug.sh
+++ .kb-plug.sh
@@ -7,6 +7,7 @@ xmodmap .xmodmap/qwerty_start
 xmodmap .xmodmaprc
 
 #xmodmap -e 'keycode 23 = Mode_switch ISO_Left_Tab Tab ISO_Left_Tab Tab ISO_Left_Tab'
+ # clicks tab two times when has it set as above
 xmodmap -e 'keycode 23 = Mode_switch'
 xmodmap -e 'keycode any = Tab'
 ~/.xcape_run.sh
--- .xcape_run.sh
+++ .xcape_run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 #killall xcape
 xcape -e \
 	'Mode_switch=Tab'
