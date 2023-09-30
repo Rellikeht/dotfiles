6073fe15052cec3001dcecd82b9fdb19cc5153e9
Rellikeht <michaelhemperek@gmail.com>
2023-09-30 18:57:34 +0200

    ocaml config done, some further linking repairs

---
+++ .config/ocaml/init.ml
@@ -0,0 +1,11 @@
+#use_output
+"[ -r ~/.config/ocaml/local.ml ] && cat ~/.config/ocaml/local.ml"
+;;
+
+(*
+
+#directory "+unix";;
+open Unix;;
+#load "unix.cma";;
+
+*)
--- diff.sh
+++ diff.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+DIFC=diff
+if [ $(uname) = "FreeBSD" ]
+then
+    DIFC=gdiff
+fi
+
+echo $DIFC
+
 for i in .[a-z]*
 do
     if [ -e "$HOME/$i" ]
     then
-        DIFF=`diff --color=always -r "$i" "$HOME/$i"`
+        DIFF=$($DIFC --color=always -r "$i" "$HOME/$i")
         if [ -n "$DIFF" ]
         then
             echo "$DIFF" | \
