0de94be4b3f6411bf5dd0a74ea4fc31af017aced
Rellikeht <michaelhemperek@gmail.com>
2023-11-26 13:36:24 +0100

    codeforces tempates yay

---
+++ .dwm/bat-linux.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export LANG=en
+
+acpi -b | \
+    sed -Ez '
+    s/.*Discharging,/BAT/;
+    s/.*[Cc]harging,/AC/g;
+    s/, ([^ ]*) .*/ \1 |/;
+    s/\n/ NOT CHARGING |/'
+
+#acpi -b | \
+#    sed -Ez 's/.* ([0-9]{1,3}%).*/\1/'
