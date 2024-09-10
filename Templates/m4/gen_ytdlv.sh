85c98b1aca425ee393f772e24f52ff8ab0b71160
Rellikeht <michaelhemperek@gmail.com>
2024-09-10 10:57:12 +0200

    change aliases to m4 generated shit

---
+++ Templates/m4/gen_ytdlv.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+SDIR="${0%/*}"
+
+m4 -DRES=360 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytdl"
+m4 -DRES=480 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytds"
+m4 -DRES=580 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytdt"
+m4 -DRES=720 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytdh"
+m4 -DRES=1080 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytdf"
+m4 -DRES=1440 "$SDIR/ytdlv.m4" >"$SDIR/../../bin/ytdq"
---
+++ Templates/m4/ytdlv.m4
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+define(HF, ifdef(`RES', RES(), 580) )dnl
+define(RF, ifdef(`ABR', ABR(), 56) )dnl
+
+LANG=
+[ -n "$YTL" ] && LANG="[language=$YTL]"
+H=HF()
+ABR=RF()
+
+exec yts -f "ba[abr>=$ABR]$LANG+bv[height>=$H]/best[height>=$H][abr>=$ABR]$LANG" $@
